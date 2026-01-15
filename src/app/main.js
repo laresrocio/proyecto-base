232cd161028441a0b75fb91aa370c21bcc257f6f
laresrocio <laresrociod@gmail.com>
2026-01-14 22:32:58 -0300

    add: inicio de funcion para buscar chats

--- src/app/main.js
+++ src/app/main.js
@@ -1,10 +1,27 @@
 //referenciar elementos a manipular del html
 const $search = document.getElementById("search")
 
-const $chats = document.querySelectorAll(".info-chat")
+const $chats = document.querySelectorAll(".chat")
 
 const searchChats = () => {
-  console.log("buscando")
+  //accedo al valor ingresado al input
+  const nameToSearch = $search.value.toLowerCase()
+
+  //por cada chat hacer esto
+  for (let i = 0; i < $chats.length; i++) {
+    //defino variable para el i
+    const $chat = $chats[i]
+    //selecciono el contenido textual del chat
+    const $contactInfo = $chat.querySelector("h3")
+    const contactName = $contactInfo.textContent.toLowerCase()
+
+    //proceso de buscado "si lo q estoy buscando aparece mostra solo eso"
+    if (contactName.includes(nameToSearch)) {
+      $chat.style.display = "flex"
+    } else {
+      $chat.style.display = "none"
+    }
+  }
 }
 
-$search.addEventListener("enter", searchChats)
+$search.addEventListener("input", searchChats)
